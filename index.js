a4e07a4ee1792fe3d7d5b02c6819f8229d59c493
iammoses <inawo.moses@gmail.com>
2023-12-16 10:04:47 +0100

    push for deploy

--- index.js
+++ index.js
@@ -41,8 +41,10 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+const PORT = process.env.PORT || 8000
+
 app.listen(8080, () => {
-  console.log('Server is running on port 8080!');
+  console.log(`Server is running on port ${PORT} !`);
 });
 
 
